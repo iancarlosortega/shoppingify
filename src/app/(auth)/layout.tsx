5037e578c1188f7de58b2c6108db1584dcf73cde
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-09-13 09:21:18 -0500

    Login Page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,35 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-	title: 'Login | Shoppingify',
-	description: 'Create your shopping list with ease and style.',
-};
+import Image from 'next/image';
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	return <>{children}</>;
+	return (
+		<main className='grid lg:grid-cols-[60%_40%] min-h-screen h-full'>
+			<section className='relative hidden lg:block'>
+				<Image
+					className='object-cover w-full h-full'
+					src='/images/login-bg.jpg'
+					alt='Examble of products in a shopping list'
+					width={1000}
+					height={1000}
+				/>
+				{/* Overlay */}
+				<div className='absolute inset-0 bg-black bg-opacity-50'>
+					<div className='absolute inset-0 flex flex-col justify-center items-center'>
+						<h1 className='text-4xl font-bold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
+							Shoppingify
+						</h1>
+						<p className='text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-center'>
+							Create your shopping list with ease and style.
+						</p>
+					</div>
+				</div>
+			</section>
+			<section className='flex justify-center items-center bg-background'>
+				{children}
+			</section>
+		</main>
+	);
 }
