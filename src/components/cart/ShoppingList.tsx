00b44af2d9525f41df6d0215ea7a2e6060bf6ebc
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-09-20 22:17:12 -0500

    Manage UI when shoppingCart has no items

--- src/components/cart/ShoppingList.tsx
+++ src/components/cart/ShoppingList.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
 import { Button, Input } from '@nextui-org/react';
 import { MdOutlineEdit } from 'react-icons/md';
 import useUIStore from '@/store/uiStore';
 import useProductStore from '@/store/productStore';
 import { BottleIcon } from '@/components/icons/BottleIcon';
 import { CategoriesList } from './CategoriesList';
-import { useForm } from 'react-hook-form';
+import { NoItemsDraw } from '../UI/NoItemsDraw';
+import { classNames } from '@/utils';
 
 interface IFormValues {
 	name: string;
@@ -32,7 +34,7 @@ export const ShoppingList = () => {
 			className={` ${
 				isShoppingListOpen ? 'translate-x-0' : 'translate-x-[400px]'
 			} w-[400px] max-w-[80%] lg:translate-x-0 flex flex-col fixed top-0 right-0 h-full transition-transform duration-300 ease-in`}>
-			<div className='p-8 flex flex-1 overflow-y-scroll flex-col bg-primary-lt dark:bg-neutral-800'>
+			<div className='p-8 flex flex-1 flex-col bg-primary-lt dark:bg-neutral-800'>
 				<header className='h-[135px] flex gap-4 bg-tertiary-dk rounded-3xl py-4 px-6 relative'>
 					<div className='absolute left-0 md:left-auto top-[-15px]'>
 						<BottleIcon />
@@ -50,18 +52,27 @@ export const ShoppingList = () => {
 					</div>
 				</header>
 
-				<main>
-					<div className='flex items-center justify-between my-6'>
-						<h3 className='text-2xl font-bold'>
-							{shoppingCart.name ? shoppingCart.name : 'Shopping List'}
-						</h3>
-						<button className='bg-transparent mr-2'>
-							<MdOutlineEdit className='h-6 w-6' />
-						</button>
-					</div>
-					<CategoriesList shoppingCart={shoppingCart} />
+				<div className='flex items-center justify-between my-6'>
+					<h3 className='text-2xl font-bold'>
+						{shoppingCart.name ? shoppingCart.name : 'Shopping List'}
+					</h3>
+					<button className='bg-transparent mr-2'>
+						<MdOutlineEdit className='h-6 w-6' />
+					</button>
+				</div>
+
+				<main className='flex-1'>
+					{shoppingCart.items.length === 0 ? (
+						<div className='h-full flex items-center justify-center relative'>
+							<p>No Items</p>
+							<NoItemsDraw className='absolute -bottom-10 left-[13%] z-10' />
+						</div>
+					) : (
+						<CategoriesList shoppingCart={shoppingCart} />
+					)}
 				</main>
 			</div>
+
 			<footer className='p-8 bg-white dark:bg-neutral-900'>
 				<form
 					autoComplete='off'
@@ -73,10 +84,14 @@ export const ShoppingList = () => {
 						placeholder='Change name'
 						color={errors.name ? 'danger' : undefined}
 						errorMessage={errors.name?.message}
+						isDisabled={shoppingCart.items.length === 0}
+						disabled={shoppingCart.items.length === 0}
+						className='disabled:!border-gray-300'
 						classNames={{
 							inputWrapper: [
 								'border border-2 border-primary dark:border-none',
 								'focus-within:!ring-0 w-[80%] focus-within:!ring-transparent pr-10',
+								'group-data-[disabled=true]:!border-gray-300',
 							],
 						}}
 						{...register('name', {
@@ -90,7 +105,11 @@ export const ShoppingList = () => {
 					<Button
 						size='lg'
 						type='submit'
-						className='bg-primary text-white font-bold absolute right-0 z-10'>
+						isDisabled={shoppingCart.items.length === 0}
+						className={classNames(
+							'bg-primary text-white font-bold absolute right-0 z-10',
+							'disabled:bg-gray-300 opacity-1'
+						)}>
 						Save
 					</Button>
 				</form>
